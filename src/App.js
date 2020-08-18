23580e1b28a3b4f8f5a24f1babde6086c8d8c6bd
David Garcia <david_5043@hotmail.com>
2020-08-17 19:58:04 -0500

    Added interaction with API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
 
 import Layout from "./components/Layout/Layout";
 
-function App() {
-  return (
-    <div className="App">
-      <Layout>
-        <p>PaperFlix esta vivo!</p>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/api/user-list/", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Layout>
+          <p>PaperFlix esta vivo!</p>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Aux from "../../hoc/Aux";
+import Aux from "../../hoc/LayoutAux";
 
 const layout = (props) => (
   <Aux>
