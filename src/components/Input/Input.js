0ceb8bb481a21ab210d3c5e16577cbba9e33c940
David Garcia <david_5043@hotmail.com>
2020-09-07 17:05:18 -0500

    added error showing messages

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,10 +3,12 @@ import "./Input.css";
 
 const Input = (props) => {
   let inputElement = null;
+  let validationError = null;
 
   let invalid = "";
   if (props.invalid && props.touched) {
     invalid = "invalid";
+    validationError = <p className="error__message">Ingresa un valor valido</p>;
   }
 
   switch (props.elementType) {
@@ -41,7 +43,12 @@ const Input = (props) => {
       );
   }
 
-  return <div className="input__container">{inputElement}</div>;
+  return (
+    <div className="input__container">
+      {inputElement}
+      {validationError}
+    </div>
+  );
 };
 
 export default Input;
